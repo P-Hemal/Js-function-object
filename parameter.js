7e9a3e1c3a4c22ff09d48cbd8b46cd5541e92e9a
P-Hemal <abdulhakim.hemal.99@gmail.com>
2024-03-02 12:23:59 +0600

    parameter updated

--- parameter.js
+++ parameter.js
@@ -1,9 +1,17 @@
-function bringTea(money){
-    console.log('Chayer dam Koto ?');
-    console.log('taka diyechen:',money)
+function bringTea(money, money2){
+    console.log('Chayer dam Koto ? =', money2);
+    console.log('taka diyechen=',money);
+    var sum = money - money2;
+    console.log("Ferot Paben =", sum);
+    
+    var money2 = 7;
+    
+
     
 }
-//  bringTea(10);
+ bringTea(50, 7);
+ 
+ 
 
 function add(num1, num2){
     console.log('going to add:', num1, num2);
@@ -11,5 +19,12 @@ function add(num1, num2){
 
 // add(110, 125);
 
+
+function sum(a, b, c, d, e){
+    console.log(a, b, c, d, e);
+    var sum = a + b + c + d + e;
+    console.log(sum)
+    
+}
  
- 
+//  sum(10, 20, 30, 40, 50);
