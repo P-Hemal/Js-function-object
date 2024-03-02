26d94be98b4226ea639736ad260a52bd33f3a79a
P-Hemal <abdulhakim.hemal.99@gmail.com>
2024-03-02 09:47:56 +0600

    parameter

---
+++ parameter.js
@@ -0,0 +1,15 @@
+function bringTea(money){
+    console.log('Chayer dam Koto ?');
+    console.log('taka diyechen:',money)
+    
+}
+//  bringTea(10);
+
+function add(num1, num2){
+    console.log('going to add:', num1, num2);
+}
+
+// add(110, 125);
+
+ 
+ 
