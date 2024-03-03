7947c61f1d6b6924dcb4666696615ec32b70afaf
P-Hemal <abdulhakim.hemal.99@gmail.com>
2024-03-03 13:36:26 +0600

    properties

---
+++ properties.js
@@ -0,0 +1,13 @@
+var shoppingCart ={
+    books : 4,
+    sunglass : 2,
+    keyboard : 2,
+    mouse : 3,
+    pen : 20
+}
+var penCount = shoppingCart.pen
+// console.log(penCount);
+var properties = Object.keys(shoppingCart);
+console.log(properties)
+var properties = Object.values(shoppingCart);
+console.log(properties)
